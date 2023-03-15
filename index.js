4c740d979b05ef1f0ffb203e66cd2cfc92baa991
sustapakhreen <sushta48@gmail.com>
2023-03-15 14:21:41 -0700

    Revised Strings Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Bruno';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
